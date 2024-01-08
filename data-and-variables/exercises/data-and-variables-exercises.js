2088d34e6e31691fa59ba04c2027ff8276398f68
alacrityj <alacritydanger@gmail.com>
2024-01-08 13:03:39 -0600

    Data and Variables Exercises completed

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,31 @@
 // Declare and assign the variables below
 
+let shuttleName = "Determination";
+let shuttleSpeed = 17500;
+let distanceToMars = 225000000;
+let distanceToMoon = 384400;
+const milesPerKilometer = 0.621;
+
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeed);
+console.log(typeof distanceToMars);
+console.log(typeof distanceToMoon);
+console.log(typeof milesPerKilometer);
 
 // Calculate a space mission below
 
+let milesToMars = distanceToMars * milesPerKilometer;
+
 // Print the results of the space mission calculations below
+
+console.log(milesToMars);
 
 // Calculate a trip to the moon below
 
+let milesToMoon = distanceToMoon * milesPerKilometer;
+
 // Print the results of the trip to the moon below
+
+console.log(milesToMoon);
