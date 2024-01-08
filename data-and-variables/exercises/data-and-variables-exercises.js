158199267335b9984c81ac87870008bcbbba2c89
alacrityj <alacritydanger@gmail.com>
2024-01-08 13:32:42 -0600

    Data and Variables exercises changes made

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -17,15 +17,19 @@ console.log(typeof milesPerKilometer);
 // Calculate a space mission below
 
 let milesToMars = distanceToMars * milesPerKilometer;
+let hoursToMars = milesToMars / shuttleSpeed;
+let daysToMars = hoursToMars /24;
 
 // Print the results of the space mission calculations below
 
-console.log(milesToMars);
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.");
 
 // Calculate a trip to the moon below
 
 let milesToMoon = distanceToMoon * milesPerKilometer;
+let hoursToMoon = milesToMoon * shuttleSpeed;
+let daysToMoon = hoursToMoon * 24; 
 
 // Print the results of the trip to the moon below
 
-console.log(milesToMoon);
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
