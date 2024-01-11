9172dbd5de2ec4d41152d0dd47121edaa7118236
alacrityj <alacritydanger@gmail.com>
2024-01-10 21:09:55 -0600

    Completed CH5 Exercises

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,4 +1,10 @@
 // Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = "green";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
@@ -9,3 +15,18 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+// Predict 
+if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+   console.log("all systems go");
+} else {
+   console.log("WARNING. Not ready");
+}
+
+if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+   console.log("WARNING. Not ready");
+} else {
+   console.log("all systems go");
+}
+
+
